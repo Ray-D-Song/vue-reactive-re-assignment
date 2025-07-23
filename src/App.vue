f9930708099ced127c4b8d906cb9f5f26a4e250e
Ray-D-Song <songraysmail@gmail.com>
2025-07-23 15:06:41 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from 'vue'
+import Form from './components/Form.vue'
+
+const form = reactive({
+  name: 'ray-d-song',
+  age: 18
+})
+</script>
+
+<template>
+  <main>
+    <Form v-model="form" />
+    <h2>{{ form.name }}</h2>
+    <h2>{{ form.age }}</h2>
+  </main>
+</template>
---
+++ src/components/Form.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { watch } from 'vue'
+
+const modelValue = defineModel()
+
+watch(modelValue.value, (newVal) => {
+  console.log(newVal)
+})
+</script>
+
+<template>
+  <div>
+    <input v-model="modelValue.name" />
+    <input v-model="modelValue.age" type="number" />
+  </div>
+</template>
